85ba62d76b6fb9cb5cbebc22ce65f997adaab62a
Parth Narkar <parthnarkarofficial@gmail.com>
2026-09-06 02:41:45 +0530

    Intro to Pizza App (without types - it just contains the utility functions in JS)

---
+++ index.js
@@ -0,0 +1,48 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+let nextOrderId = 1;
+console.log(nextOrderId);
+
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
+
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId, pizza: selectedPizza, status: "ordered" }
+    nextOrderId++;
+    console.log(nextOrderId);
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+/**
+ * Challenge: write another utility function, completeOrder, that takes an orderId as a parameter
+ * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
+ * return the found order from the function.
+ * 
+ * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
+ */
+
+function completeOrder(orderId) {
+    let selectedOrder = orderQueue.find((item) => item.id === orderId);
+    selectedOrder.status = "completed";
+    return selectedOrder;
+}
