588c953209b04dc0a3330ff10bbe3abb0f95f7f5
Parth Narkar <parthnarkarofficial@gmail.com>
2026-09-06 13:19:51 +0530

    (using Type Narrowing) implemente getPizzaDetail

--- script.ts
+++ script.ts
@@ -54,6 +54,26 @@ function completeOrder(orderId: number) {
     return order
 }
 
+// NORMAL LOGIC
+// function getPizzaDetail(identifier: string | number) {
+//     return menu.find((item: Pizza) => {
+//         return item.id === identifier || item.name === identifier;
+//     });
+// }
+
+/*
+TYPE NARROWING: in the below we have implemented the type narrowing, like when we don't know what the data type of the identifier will be, then in the IF condition put the string condition , then we narrow down to number
+*/
+export function getPizzaDetail(identifier: string | number) {
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+    } else if (typeof identifier === "number") {
+        return menu.find(pizza => pizza.id === identifier)
+    } else {
+        throw new TypeError("Parameter `identifier` must be either a string or a number")
+    }
+}
+
 addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
 addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
 addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
@@ -64,3 +84,6 @@ completeOrder(1);
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order queue:", orderQueue)
+
+// console.log(getPizzaDetail("Margherita"));
+// console.log(getPizzaDetail(2));
