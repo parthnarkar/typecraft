a2da322c64bcf3071e425ef34d6fff056dd43e96
Parth Narkar <parthnarkarofficial@gmail.com>
2026-09-06 15:39:18 +0530

    Adding automatic IDs to Pizza items in the menu (but the code still have type errors

--- script.ts
+++ script.ts
@@ -12,11 +12,13 @@ type Order = {
     status: OrderStatus
 };
 
+let nextPizzaId = 1;
+
 const menu: Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
 ]
 
 let cashInRegister = 100
@@ -26,8 +28,12 @@ let nextOrderId = 1
 const orderQueue: Order[] = []
 
 function addNewPizza(pizzaObj: Pizza): void {
-    menu.push(pizzaObj)
+    menu.push({ id: nextPizzaId++, ...pizzaObj})
 }
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 function placeOrder(pizzaName: string): Order | undefined {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
@@ -64,7 +70,7 @@ function completeOrder(orderId: number): Order | undefined {
 /*
 TYPE NARROWING: in the below we have implemented the type narrowing, like when we don't know what the data type of the identifier will be, then in the IF condition put the string condition , then we narrow down to number
 */
-export function getPizzaDetail(identifier: string | number) {
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
     if (typeof identifier === "string") {
         return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
     } else if (typeof identifier === "number") {
@@ -74,16 +80,12 @@ export function getPizzaDetail(identifier: string | number) {
     }
 }
 
-addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
-
-placeOrder("Chicken Bacon Ranch")
-completeOrder(1);
+// placeOrder("Chicken Bacon Ranch")
+// completeOrder(1);
 
 console.log("Menu:", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
 
 // console.log(getPizzaDetail("Margherita"));
 // console.log(getPizzaDetail(2));
